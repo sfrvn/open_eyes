72c1a039defb76cf2eb886f1a535933be24026a6
Бунёд <klmtv@icloud.com>
2021-02-06 15:03:04 +0300

    Documentation

--- open_eyes/open_eyes/urls.py
+++ open_eyes/open_eyes/urls.py
@@ -3,11 +3,15 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
 
+from .yasg import urlpatterns as doc_urls
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/', include('charity.urls')),
 ]
+
+urlpatterns += doc_urls
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
